8bd12701083cb9b0863e3a2ee0860c449f477366
lunasara <jfslumbreras@gmail.com>
2019-12-17 21:39:48 +0100

    CORRECT apply_h function

--- week1/apply_H.m
+++ week1/apply_H.m
@@ -1,35 +1,29 @@
-function [ output_image ] = apply_H( image, H )
+function [ output_image, x_min, x_max, y_min, y_max  ] = apply_H( image, H )
 % Applies an homography H to an image 
     %Homogeneous coordinates of the corner points of the image
     size_image = size(image);
-%     p =[1 1 1; 1 size_image(1) 1; size_image(2) 1 1; size_image(2) size_image(1) 1]';
     p1 = [1; 1; 1];
     p2 = [1; size_image(1); 1];
     p3 = [size_image(2); 1; 1];
     p4 = [size_image(2); size_image(1); 1];
     
     %Compute location of the corners in the output image
-%     po = H*p;
     p1_output_h = H * p1;
     p2_output_h = H * p2;
     p3_output_h = H * p3;
     p4_output_h = H * p4;
     
     %Transform the output corner points to euclidean coordinates
-%     po = po./repmat(po(3,:),3,1);
     p1_output_e = [p1_output_h(1)/p1_output_h(3) p1_output_h(2)/p1_output_h(3)];
     p2_output_e = [p2_output_h(1)/p2_output_h(3) p2_output_h(2)/p2_output_h(3)];
     p3_output_e = [p3_output_h(1)/p3_output_h(3) p3_output_h(2)/p3_output_h(3)];
     p4_output_e = [p4_output_h(1)/p4_output_h(3) p4_output_h(2)/p4_output_h(3)];
     
     %Create the output image
-    %minim = min(po,[],2)
-    %maxim = max(po,[],2)
     x_min = min([p1_output_e(1) p2_output_e(1) p3_output_e(1) p4_output_e(1)]);
     x_max = max([p1_output_e(1) p2_output_e(1) p3_output_e(1) p4_output_e(1)]);
     y_min = min([p1_output_e(2) p2_output_e(2) p3_output_e(2) p4_output_e(2)]);
     y_max = max([p1_output_e(2) p2_output_e(2) p3_output_e(2) p4_output_e(2)]);
-    %output_image = zeros(ceil(y_max-y_min), ceil(x_max-x_min), 3);
     
     %Indirect mapping
     [X,Y] = meshgrid(1:size_image(2), 1:size_image(1));
