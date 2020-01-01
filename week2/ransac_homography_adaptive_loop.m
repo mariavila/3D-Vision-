b4d3ceb8c4462485baeb286b0f375be889ff263f
Josep M Vila <mariava.1213@gmail.com>
2020-01-01 14:15:22 +0100

    Fasten and cleaned code of homography2d and castle mosaic done

--- week2/ransac_homography_adaptive_loop.m
+++ week2/ransac_homography_adaptive_loop.m
@@ -8,7 +8,7 @@ best_inliers = [];
 while it < max_it
     
     points = randomsample(Npoints, 4);
-    H = homography2d_2nd(x1(:,points), x2(:,points)); % ToDo: you have to create this function
+    H = homography2d(x1(:,points), x2(:,points)); % ToDo: you have to create this function
     inliers = compute_inliers(H, x1, x2, th);
     
     % test if it is the best model so far
@@ -29,7 +29,7 @@ while it < max_it
 end
 
 % compute H from all the inliers
-H = homography2d_2nd(x1(:,best_inliers), x2(:,best_inliers));
+H = homography2d(x1(:,best_inliers), x2(:,best_inliers));
 idx_inliers = best_inliers;
 
 
@@ -51,7 +51,6 @@ function idx_inliers = compute_inliers(H, x1, x2, th)
         d2_2 = (x2(1,i)/x2(3,i) - x1p(1)/x1p(3))^2+(x2(2,i)/x2(3,i) - x1p(2)/x1p(3))^2;
         d2(i) =(d2_1 + d2_2);  
     end
-    disp(min(d2(:)))
     idx_inliers = find(d2 < th.^2);
     
 
@@ -69,53 +68,16 @@ function item = randomsample(npts, n)
 	  item(i) = a(r);       % Select the rth element from the list
 	  a(r)    = a(end-i+1); % Overwrite selected element
     end                       % ... and repeat
-
-% function homo = homography2d(x1,x2)
-%     n_points = size(x1);
-%     n_points = n_points(2);
-%     x1p = normalise(x1);
-%     x2p = normalise(x2);
-%     mean1 = mean(x1,2);
-%     mean2 = mean(x2,2);
-%     std1 = std(x1,1,2);
-%     std2 = std(x2,1,2);
-%     s1 = std1./sqrt(2);
-%     s2 = std2./sqrt(2);
-%     T1 = [s1(1), 0, -mean1(1);...
-%           0, s1(2), -mean1(2);...
-%           0, 0, 1];
-%     T2 = [s2(1), 0, -mean2(1);...
-%           0, s2(2), -mean2(2);...
-%           0, 0, 1];
-%     x1p = T1 * x1p;
-%     x2p = T2 * x2p;
-%     count = 1;
-%     for i = 1:n_points
-%         x1i = x1p(:,i);
-%         x2i = x2p(:,i);
-%         A(count,:) = [0, 0, 0,...
-%                      -x2i(3)*x1i(1), -x2i(3)*x1i(2), -x2i(3)*x1i(3), ...
-%                       x2i(2).*x1i(1), x2i(2).*x1i(2), x2i(2).*x1i(3)];
-%                   
-%         A(count+1,:) = [x2i(3)*x1i(1), x2i(3)*x1i(2), x2i(3)*x1i(3), ...
-%                         0, 0, 0,...
-%                         x2i(1)*x1i(1),x2i(1)*x1i(2),x2i(1)*x1i(3)];
-%         count = count + 2;
-%     end
-%     [U,D,V] = svd(A);
-%     h = V(9,:);
-%     homo = reshape(h,3,3);
-%     homo = inv(T2)*homo*T1;
     
-function homo = homography2d_2nd(x1,x2)
+function homo = homography2d(x1,x2)
     n_points = size(x1);
     n_points = n_points(2);
     x1p = normalise(x1);
     x2p = normalise(x2);
-    mean1 = mean(x1,2);
-    mean2 = mean(x2,2);
-    std1 = std(x1,1,2);
-    std2 = std(x2,1,2);
+    mean1 = mean(x1p,2);
+    mean2 = mean(x2p,2);
+    std1 = std(x1p,1,2);
+    std2 = std(x2p,1,2);
     s1 = std1./sqrt(2);
     s2 = std2./sqrt(2);
     T1 = [s1(1), 0, -mean1(1);...
@@ -141,7 +103,6 @@ function homo = homography2d_2nd(x1,x2)
     h = V(:,end);
     homo = reshape(h,3,3);
     homo = homo';
-    homo = inv(T2)*homo*T1;
+    homo = T2\homo;
+    homo = homo*T1;
     homo = homo./homo(3,3);
-    %homo = T2\homo
-    %homo = homo*T1;
